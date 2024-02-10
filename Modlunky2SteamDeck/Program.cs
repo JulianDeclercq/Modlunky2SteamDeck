306dbb2e9641ae2b6664e8702073154de24d84bc
Julian Declercq <julian.declercq1@gmail.com>
2024-02-10 02:09:21 +0100

    First version that adds the shortcut to Steam

---
+++ Modlunky2SteamDeck/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using ValveKeyValue;
+
+namespace Modlunky2SteamDeck;
+
+internal abstract class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Modlunky2SteamDeck started");
+        const string steamPath = "/home/deck/.local/share/Steam/userdata";
+
+        // TODO: Support entering your steam id, this just picks the first one.
+        var userPath = Directory.GetDirectories(steamPath)[0];
+        var shortcutsPath = $"{userPath}/config/shortcuts.vdf";
+
+        // TODO: REMOVE ME
+        //shortcutsPath = "../../../shortcuts.vdf";
+
+        List<Shortcut> shortcuts;
+        var binarySerializer = KVSerializer.Create(KVSerializationFormat.KeyValues1Binary);
+        using (var inputStream = File.OpenRead(shortcutsPath))
+        {
+            shortcuts = binarySerializer.Deserialize<List<Shortcut>>(inputStream);
+        }
+        Console.WriteLine("Successfully read existing shortcuts from stream");
+
+        Shortcut? modlunkyEntry;
+        var assembly = Assembly.GetExecutingAssembly();
+        using (var modlunkyStream = assembly.GetManifestResourceStream("Modlunky2SteamDeck.modlunky_shortcut.vdf"))
+        {
+            modlunkyEntry = binarySerializer.Deserialize<Shortcut>(modlunkyStream);
+            if (modlunkyEntry == null)
+                throw new Exception("Failed to read modlunky entry");
+        }
+
+        Console.WriteLine("Successfully read modlunky entry from resource file.");
+
+        if (shortcuts.Any(s => s.exe.Equals(modlunkyEntry.exe)))
+            throw new Exception("Shortcut with modlunky exe already exists.");
+
+        shortcuts.Add(modlunkyEntry);
+
+        using var outputStream = File.OpenWrite(shortcutsPath);
+        binarySerializer.Serialize(outputStream, shortcuts, ""); // not sure what to use for name
+        Console.WriteLine("Successfully wrote new shortcut to stream");
+    }
+}
---
+++ Modlunky2SteamDeck/Shortcut.cs
@@ -0,0 +1,27 @@
+﻿namespace Modlunky2SteamDeck;
+
+public class Shortcut
+{
+    public string appId { get; set; }
+    public string appname { get; set; } = "";
+    public string exe { get; set; } = "";
+    public string StartDir { get; set; } = "";
+    public string icon { get; set; } = "";
+    public string ShortCutPath { get; set; } = "";
+    public string LaunchOptions { get; set; } = "";
+    public bool IsHidden { get; set; }
+    public bool AllowDesktopConfig { get; set; }
+    public bool AllowOverlay { get; set; }
+    public bool OpenVR { get; set; }
+    public bool Devkit { get; set; }
+    public string DevkitGameID { get; set; } = "";
+    public bool DevkitOverrideAppID { get; set; }
+    public int LastPlayTime { get; set; } // datetime but not sure how to parse (unix timestamp so int instead?)
+    public string FlatpakAppID { get; set; } = "";
+    public List<string> Tags { get; set; } = new();
+
+    public override string ToString()
+    {
+        return $"{appname} ({exe}) {appId}";
+    }
+}
