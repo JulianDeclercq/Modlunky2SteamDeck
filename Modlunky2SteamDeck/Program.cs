437d521d0455c09bc7646b3ed459b9b836f0996a
Julian Declercq <julian.declercq1@gmail.com>
2024-02-13 21:43:02 +0100

    program flow

--- Modlunky2SteamDeck/Program.cs
+++ Modlunky2SteamDeck/Program.cs
@@ -11,9 +11,6 @@ internal abstract class Program
     {
         Console.WriteLine("Modlunky2SteamDeck started");
 
-        AddCompatToolMapping("./config.vdf");
-        return;
-        
         const string steamPath = "/home/deck/.local/share/Steam";
         const string configPath = $"{steamPath}/config/config.vdf";
 
@@ -32,15 +29,13 @@ internal abstract class Program
         shortcuts.Add(modlunkyEntry);
 
         SaveShortcuts(binarySerializer, shortcuts, shortcutsPath);
-        //AddCompatToolMapping(configPath);
+        AddCompatToolMapping(configPath);
 
         Console.WriteLine("Modlunky2SteamDeck finished successfully.");
     }
 
     private static List<Shortcut> LoadShortcuts(KVSerializer binarySerializer, string shortcutsPath)
     {
-        //shortcutsPath = "../../../shortcuts.vdf";
-
         using var shortcutsInputStream = File.OpenRead(shortcutsPath);
         var shortcuts = binarySerializer.Deserialize<List<Shortcut>>(shortcutsInputStream);
 
@@ -78,16 +73,18 @@ internal abstract class Program
         };
         options.Conditions.Clear(); // Remove default conditionals set by the library
 
-        var backupPath = $"{configPath}{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}.backup";
-        File.Copy(configPath, backupPath); 
-        
         using var configInputStream = File.OpenRead(configPath);
         var config = textSerializer.Deserialize<InstallConfigStore>(configInputStream, options);
         
-        // var modlunkyAppId = "3921648026"; // real one
-        var modlunkyAppId = "1337"; // fake one for testing
+        const string modlunkyAppId = "3921648026"; 
 
         var compatToolMapping = config.Software.Valve.Steam.CompatToolMapping;
+        if (compatToolMapping.ContainsKey(modlunkyAppId))
+        {
+            Console.WriteLine("Modlunky compat tool mapping already exists");
+            return;
+        }
+        
         compatToolMapping[modlunkyAppId] = new CompatToolMappingEntry
         {
             name = "proton_experimental",
@@ -95,57 +92,13 @@ internal abstract class Program
             priority = 250
         };
         
-        // TODO: write to the configPath, not to the test output
-        using var stream = new FileStream("configtestout.vdf", FileMode.Truncate, FileAccess.Write, FileShare.None);
+        var backupPath = $"{configPath}{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}.backup";
+        File.Copy(configPath, backupPath); 
+        
+        using var stream = new FileStream(configPath, FileMode.Truncate, FileAccess.Write, FileShare.None);
         {
             var kv = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
             kv.Serialize(stream, config, "InstallConfigStore");
         }
-        return;
-        
-        var newSerializer = KVSerializer.Create(KVSerializationFormat.KeyValues1Text); 
-        var newOptions = new KVSerializerOptions
-        {
-            HasEscapeSequences = true
-        };
-        newOptions.Conditions.Clear(); // Remove default conditionals set by the library
-        
-        using var newConfigInputStream = File.OpenRead(configPath);
-        var newConfig = newSerializer.Deserialize(newConfigInputStream, newOptions);
-        int bkpt = 5;
-    }
-
-    private static void AddCompatToolMappingOld(string configPath)
-    {
-        var textSerializer = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
-        var options = new KVSerializerOptions
-        {
-            HasEscapeSequences = true
-        };
-        options.Conditions.Clear(); // Remove default conditionals set by the library
-
-        using var configInputStream = File.OpenRead(configPath);
-        var config = textSerializer.Deserialize(configInputStream, options);
-
-        // For some reason, using Children is the way to go since then the return type is KVObject instead of KVValue when using e.g. config["Software"]
-        var newSoftware = config.Children.Single(c => c.Name == "Software");
-        var newValve = newSoftware.Children.Single(c => c.Name == "Valve");
-        var newSteam = newValve.Children.Single(c => c.Name == "Steam");
-        var newCompatToolMapping = newSteam.Children.Single(c => c.Name == "CompatToolMapping");
-
-        // var modlunkyAppId = "3921648026"; // real one
-        var modlunkyAppId = "1337"; // fake one for testing
-
-        var modlunkyEntry = new KVObject(modlunkyAppId, new List<KVObject>
-        {
-            new("name", "proton_experimental"),
-            new("config", ""),
-            new("priority", 250)
-        });
-        newCompatToolMapping.Add(modlunkyEntry);
-
-        using var outputStream = File.OpenWrite($"{configPath}TESTOUTPUT");
-        textSerializer.Serialize(outputStream, config, ""); // not sure about name
-        Console.WriteLine("Successfully wrote modlunky compat tool mapping to stream");
     }
 }
