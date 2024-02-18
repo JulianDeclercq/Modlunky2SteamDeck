7f91c224b4832989613fe5a249d819c37a57f083
Julian Declercq <julian.declercq1@gmail.com>
2024-02-18 20:52:09 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -4,30 +4,30 @@ echo "Steam needs to be shutdown in order for Modlunky2SteamDeck to run. Shuttin
 steam -shutdown &
 
 while pgrep -x steam >/dev/null; do
-    echo "Waiting for Steam to shut down..."
-    sleep 2
+    echo "Steam is being shut down, please wait.."
+    sleep 3
 done
 
 echo "Steam has been shut down, continuing install of Modlunky2SteamDeck"
 
-# GitHub API URL for the latest release
+# Fetch the latest Modlunky2SteamDeck release
 api_url="https://api.github.com/repos/JulianDeclercq/Modlunky2SteamDeck/releases/latest"
 echo "Fetching latest release data from $api_url"
 
 # Use curl to fetch the latest release data and jq to parse the JSON for the browser_download_url
 download_url=$(curl -s $api_url | jq -r '.assets[0].browser_download_url')
 
-# Check if the download URL is not empty
 if [ -z "$download_url" ]; then
     echo "Download URL could not be found."
     exit 1
 fi
 
-# Use wget or curl to download the latest release
+# Download the release
 echo "Downloading latest release from $download_url"
 curl -LO $download_url
 
 echo "Download of Modlunky2SteamDeck completed, running now."
 
+# Execute the release
 chmod +x Modlunky2SteamDeck
 ./Modlunky2SteamDeck
